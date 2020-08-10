f15e16cd98679266b76bf1230edb91a7d05712a1
Daniel Gröber <dxld@darkboxed.org>
2020-08-10 07:02:54 +0200

    Remove vendored iproute package

--- iproute/Data/IP/Internal.hs
+++
@@ -1,7 +0,0 @@
-module Data.IP.Internal ( IPv4(..)
-                        , IPv6(..)
-                        , AddrRange(..)
-                        ) where
-
-import Data.IP.Addr
-import Data.IP.Range
--- iproute/test/doctests.hs
+++
@@ -1,6 +0,0 @@
-module Main where
-
-import Test.DocTest
-
-main :: IO ()
-main = doctest ["-XOverloadedStrings", "Data/IP.hs", "Data/IP/RouteTable.hs"]
